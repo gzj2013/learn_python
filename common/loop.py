6584cdb258d47e4af003efdfc8c48e70845d22cc
Zack <guozhijie_2006@126.com>
2017-07-10 18:50:24 +0800

    add loop.py

--- common/loop.py
+++ common/loop.py
@@ -4,7 +4,7 @@
 # Python的循环有两种，一种是for...in循环，依次把list或tuple中的每个元素迭代出来
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
-    print(name)
+    print('Hello %s!' % name)
 
 sum = 0
 for x in [1,2,3,4,5,6,7,8,9]:
@@ -22,4 +22,13 @@ print(sum)
 print(list(range(5)))
 
 
+
+# 第二种循环是while循环，只要条件满足，就不断循环，条件不满足时退出循环。
+# 比如我们要计算100以内所有奇数之和，可以用while循环实现
+sum = 0
+n = 99
+while n > 0:
+    sum += n
+    n -= 2
+print(sum)
 
